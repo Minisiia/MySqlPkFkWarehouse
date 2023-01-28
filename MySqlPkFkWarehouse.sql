79dec03ec1d2aa9a6813faeae09eefc0080cfff8
Anastasiia <113467640+Minisiia@users.noreply.github.com>
2023-01-28 21:36:28 +0200

    Update MySqlPkFkWarehouse.sql

--- MySqlPkFkWarehouse.sql
+++ MySqlPkFkWarehouse.sql
@@ -31,17 +31,17 @@ CREATE TABLE suppliers(
 INSERT INTO suppliers
 (organization_name, site, contact_person, phone, email, city, department_id)
 VALUES  ('Yummy shop','yummyshop.com','John Doe','+380970000001','john_Doe@Yummyshop.com','London', 2),
-		('Comp shop','compshop.com','Jane Dolle','+380550000025','jane_Dolle@compshop.com','Milan', 1),
+	('Comp shop','compshop.com','Jane Dolle','+380550000025','jane_Dolle@compshop.com','Milan', 1),
         ('Beauty shop','bs.com','Ola Smith','+380900088401','ola_smith@bs.com','Kyiv', 3),
         ('Candy shop','c-shop.com','Sunny Funny','+380660000001','sf@c-shop.com','Kharhiv', 2);
         
 CREATE TABLE clients(
-	id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     organization_name VARCHAR(20),
     site VARCHAR(25),
     contact_person VARCHAR(50),
     phone VARCHAR(15),
-	email VARCHAR(50),
+    email VARCHAR(50),
     city VARCHAR(15),
     department_id INT,
     FOREIGN KEY(department_id) REFERENCES department(id),
@@ -51,15 +51,15 @@ INSERT INTO clients
 (organization_name, site, contact_person, phone, email, city, department_id)
 VALUES  ('Shop 1','blabla.com','Serg Drew','+38085222001','milashka@blabla.com','Mukachevo', 1),
         ('EVA','eva.com.ua','Olga Vorkova','+38019846527','o_vork@eva.com.ua','Kyiv', 3),
-		('ABC',NULL,'Marichka Ivanko','+380561591597',NULL,'Svaliava', 2);
+	('ABC',NULL,'Marichka Ivanko','+380561591597',NULL,'Svaliava', 2);
 	     
 CREATE TABLE staff(
-	id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(20),
     last_name  VARCHAR(20),
     sex VARCHAR(7),
     phone VARCHAR(15),
-	email VARCHAR(50),
+    email VARCHAR(50),
     city VARCHAR(15),
     birth_date DATE,
     position VARCHAR(25),
@@ -76,7 +76,7 @@ VALUES  ('Angelina','Vovk','female','+380952020121','milashka_lkj@gmail.com','Lv
         ('Ivan','Docenko','male','+38095464221','asfkj@gmail.com','Uman', '1985-05-01', 'Storekeeper','2004-12-02',NULL,2),
         ('Olga','Finko','female','+380952050121','sdd@gmail.com','Dnipro', '1998-01-09', 'Storekeeper','2020-04-02',NULL,1),
         ('Lina','Likova','female','+380954020121','lina@gmail.com','Lviv', '2000-12-15', 'Storekeeper','2012-02-02',NULL,3),
-		('Anton','Inov','male','+380504020121','ant_in@gmail.com','Chernigiv', '1960-12-12', 'Director','2000-01-02',NULL,4);
+	('Anton','Inov','male','+380504020121','ant_in@gmail.com','Chernigiv', '1960-12-12', 'Director','2000-01-02',NULL,4);
 
 SELECT * FROM staff;  
 SELECT * FROM clients;
